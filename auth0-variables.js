6991e7ec3ee5e8e61931dcdc13257f88b8152323
karen <kkwentus@chef.io>
2018-10-16 10:13:43 -0500

    heroku

--- auth0-variables.js
+++ auth0-variables.js
@@ -2,4 +2,4 @@ var AUTH0_CLIENT_ID='3z16EouEwgpMHghii1LQ7OMTzRgH0i5D';
 var AUTH0_DOMAIN='kkwen.auth0.com';
 var AUTH0_CALLBACK_URL=location.href;
 var AUTH0_API_URL='https://fathomless-badlands-76591.herokuapp.com';
-var AUTH0_AUDIENCE'HIPSTERBANKURL';
+var AUTH0_AUDIENCE='HIPSTERBANKURL';
